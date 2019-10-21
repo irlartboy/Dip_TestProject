3a2186eb8fad568d6c8ba0a2b9bd579869e5f8f5
irlartboy <ollie_king99@outlook.com>
2019-10-21 15:15:28 +1100

    Unity Test added

---
+++ Assets/Tests/SimpleTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class SimpleTests 
+{
+   [Test]
+   public void NUnitTest()
+   {
+        Assert.IsTrue(false, "NUnit Test Failed");
+   }
+    public IEnumerator SimpleUnityTestTools()
+    {
+        yield return new WaitForFixedUpdate();
+        Assert.IsTrue(false, "Unity Test Failed")
+    }
+}
